61bd2422a507fd1e56560374c0a3b403aacdffa7
berrondo <claudio.berrondo@gmail.com>
2021-07-29 04:09:32 -0300

    configura pytest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,12 @@
 import pytest
+from rest_framework.test import APIClient
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access(db):
     pass
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
